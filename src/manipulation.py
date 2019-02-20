e3a06f70c6d1b2e6673f339401879af0a5acec3d
Zhicheng Yu <zhichengyu2019@u.northwestern.edu>
2019-02-20 17:51:41 -0600

    add_shared_files

--- src/manipulation.py
+++ src/manipulation.py
@@ -4,11 +4,13 @@ import rospy
 import numpy as np
 from std_msgs.msg import Float64
 from geometry_msgs.msg import Twist
+from ar_track_alvar_msgs.msg import AlvarMarkers
 
 class Manipulation():
     def __init__(self):
 
         self.pub_cmd_vel = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
+        self.sub_info_marker = rospy.Subscriber('ar_pose_marker',AlvarMarkers,self.head_to_artag, queue_size = 1)
 
         rospy.on_shutdown(self.fnShutDown)
 
@@ -25,6 +27,40 @@ class Manipulation():
         twist.angular.z = 0
         self.pub_cmd_vel.publish(twist)
 
+    def head_to_artag(self,data):
+        rospy.loginfo("Subscribing the ar_pose")
+
+
+        twist = Twist()
+        twist.linear.x = 0
+        twist.linear.y = 0
+        twist.linear.z = 0
+        twist.angular.x = 0
+        twist.angular.y = 0
+        twist.angular.z = 0
+
+        rospy.sleep(1)
+        while(data.markers[0].pose.pose.position.y<-0.2 or data.markers[0].pose.pose.position.y>0.2):
+            twist.angular.z = data.markers[0].pose.pose.position.y
+            print(twist)
+            self.pub_cmd_vel.publish(twist)
+        twist.angular.z = 0
+        self.pub_cmd_vel.publish(twist)
+
+        rospy.loginfo("angle correct")
+
+        while(data.markers[0].pose.pose.position.x>0.1):
+            twist.linear.x = 0.08
+            print("start to move in x")
+            self.pub_cmd_vel.publish(twist)
+
+        twist.linear.x = 0
+        self.pub_cmd_vel.publish(twist)
+
+        rospy.loginfo("Have moved to artag!")
+
+
+    '''
     def parking_test(self):
 
         rospy.sleep(1)
@@ -69,6 +105,8 @@ class Manipulation():
         twist.angular.z = 0
         self.pub_cmd_vel.publish(twist)
         rospy.sleep(3)
+    '''
+
 
 
 
@@ -79,7 +117,7 @@ def main():
 
     rospy.init_node('manipulation_test')
     mani = Manipulation()
-    mani.parking_test()
+    #mani.parking_test()
     rospy.loginfo('manipulation test begins!')
 
     while not rospy.is_shutdown():

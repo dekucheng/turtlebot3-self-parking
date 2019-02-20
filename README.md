bd057e634f524bc1b226f09a95407134a0cd5db5
Zhicheng Yu <zhichengyu2019@u.northwestern.edu>
2019-02-20 10:14:54 -0600

    update_cv_test

--- README.md
+++ README.md
@@ -51,9 +51,22 @@ Then in Remote PC, run:
 1. solve the low fps problem by using publish a compressed image and small queue_size.
 2. calibration finished, rectified seems well (needs test in real-world)
 3. go through the tutorial that how opencv communicate with ROS.
-4. CompressedImage msg type seems cannot be transformed by cv_bridge, mannually extract the data from compressedImage to fit into opencv for further operation.
+3. CompressedImage msg type seems cannot be transformed by cv_bridge, mannually extract the data from compressedImage to fit into opencv for further operation.
 
 ### Things to do:
 1. Check if the calibration works well for bird's eye view camera.
 2. find proper algorithm to send back the location of detected line.
 3. find out urdf relations between camera frame and base frame to navigation.
+
+
+## Week 7:
+Run the camera:
+>**$  $ roslaunch turtlebot3_autorace_camera turtlebot3_autorace_camera_pi.launch**
+
+Run the rectified camera:
+>**roslaunch turtlebot3_autorace_camera turtlebot3_autorace_intrinsic_camera_calibration.launch**
+
+The rectified image is published in topic **/camera/image_rect_color/compressed**
+
+Bringup robot to enable multiple devices (compatible with camera launch file):
+>**roslaunch turtlebot3_bringup turtlebot3_robot.launch**
--- src/cvtest.py
+++ src/cvtest.py
@@ -9,17 +9,22 @@ from scipy.ndimage import filters
 from std_msgs.msg import String
 from sensor_msgs.msg import Image,CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
+from turtlebot3_selfparking.srv import*
+from std_srvs.srv import Trigger
 import numpy as np
 
 VERBOSE = True
 
-class image_converter:
+class image_receiver():
 
     def __init__(self):
         self.image_pub = rospy.Publisher("image_topic_1",CompressedImage,queue_size=10)
 
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/camera/image/compressed",CompressedImage,self.callback)
+        self.image_sub = rospy.Subscriber("/camera/image_rect_color/compressed",CompressedImage,self.callback)
+        self.call_init_image = rospy.ServiceProxy('cv_test/provide_rectified_image', Trigger)
+        rospy.Service('cv_test/provide_rectified_image',Trigger,self.svc_set_init_image)
+        self.image_np = CompressedImage()
 
     def callback(self, ros_data):
         '''Callback function of subscribed topic.
@@ -28,36 +33,60 @@ class image_converter:
             print('received image of type: "%s"' % ros_data.format)
 
         #### direct conversion to CV2 ####
+        self.image_np = ros_data.data
+
+        '''
         np_arr = np.fromstring(ros_data.data, np.uint8)
-        image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        self.image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
         #image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) # OpenCV >= 3.0:
 
         #### Feature detectors using CV2 ####
         # "","Grid","Pyramid" +
         # "FAST","GFTT","HARRIS","MSER","ORB","SIFT","STAR","SURF"
         '''
-        cv2.imshow('cv_img', image_np)
-        print(image_np.shape)
-        cv2.waitKey(2)
         '''
         #### Create CompressedIamge ####
         msg = CompressedImage()
         msg.header.stamp = rospy.Time.now()
         msg.format = "jpeg"
-        msg.data = np.array(cv2.imencode('.jpg', image_np)[1]).tostring()
+        msg.data = np.array(cv2.imencode('.jpg', self.image_np)[1]).tostring()
         # Publish new image
         self.image_pub.publish(msg)
+        '''
+
 
         #self.subscriber.unregister()
+    def svc_set_init_image(self, data):
+
+        np_arr = np.fromstring(self.image_np, np.uint8)
+        init_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+        print(init_image.shape)
+        rospy.sleep(1)
+        cv2.imshow('cv_img', init_image)
+
+        cv2.imwrite('parking_test_02.jpg',init_image)
+
+        cv2.waitKey(0)
+
+
+        #if rospy.ROSInterruptException:
+        #    cv2.destroyAllWindows()
+        #    print("cv_shutting_down")
+
+        return (True,"Has recerived the image")
 
 def main():
-  ic = image_converter()
-  rospy.init_node('image_converter', anonymous=True)
+  rospy.init_node('cv_test')
+
+  ic = image_receiver()
+  rospy.sleep(1)
+  ic.call_init_image()
   try:
     rospy.spin()
   except KeyboardInterrupt:
     print("Shutting down")
-  cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
